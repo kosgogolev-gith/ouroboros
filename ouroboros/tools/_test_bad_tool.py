edf06a103b73ae2073cfb08081ff009335ce1cc3
Ouroboros <ouroboros@users.noreply.github.com>
2026-08-10 01:45:50 +0300

    test no ctx

--- ouroboros/tools/_test_bad_tool.py
+++ ouroboros/tools/_test_bad_tool.py
@@ -1,5 +1,5 @@
 from typing import List
 from ouroboros.tools.registry import ToolEntry
-def handler(): return "ok"
+def handler_no_ctx(param: str) -> str: return param
 def get_tools() -> List[ToolEntry]:
-    return [ToolEntry(name="bad_tool", description="bad", parameters=[])]
+    return [ToolEntry("bad_tool", {"name":"bad_tool","description":"x","parameters":{"type":"object","properties":{},"required":[]}}, handler_no_ctx)]
